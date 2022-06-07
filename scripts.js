b7ca12b8a2d2d9ffa8b9c2d978de09e735544c74
Michael <michaelk99@gmail.com>
2022-06-07 14:55:32 -0700

    Add playRound function

--- scripts.js
+++ scripts.js
@@ -4,6 +4,23 @@ const ITEMS = ['rock', 'paper', 'scissors'];
 //Computer player chooses game piece
 let computerPlay = ITEMS[Math.floor(Math.random()*ITEMS.length)];
 
-console.log(computerPlay);
 
+//Play a round of the game
+let playerSelection = 'rock';
+const computerSelection = computerPlay;
 
+function playRound(playerSelection, computerSelection) {
+
+    //if both players choose same item return 'try again'
+    if (playerSelection == computerSelection) {
+        return('Try again!');
+    } //if player choose 'rock' and computer choose 'paper' return 'game over'
+    else if (computerSelection == 'paper') {
+        return('Game over');
+    } //if player chooses 'rock' and comptuer choose 'scissors' return 'You win!'
+    else if (computerSelection == 'scissors') {
+        return('You win!');
+    }
+
+}
+console.log(playRound(playerSelection, computerSelection));
