f28a40afb0169fc667465c53560413f4377e115f
Michael <michaelk99@gmail.com>
2022-06-07 14:14:05 -0700

    Add computerPlay function

---
+++ scripts.js
@@ -0,0 +1,9 @@
+//Array with game pieces
+const ITEMS = ['rock', 'paper', 'scissors'];
+
+//Computer player chooses game piece
+let computerPlay = ITEMS[Math.floor(Math.random()*ITEMS.length)];
+
+console.log(computerPlay);
+
+
