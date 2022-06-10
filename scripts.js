7a3e51493e5c047ace2ac09f4a004aa3b492585e
Michael <michaelk99@gmail.com>
2022-06-10 16:05:59 -0700

    Add case insentivity and playerSelection

--- scripts.js
+++ scripts.js
@@ -1,57 +1,27 @@
-//Array with game pieces
 const ITEMS = ['rock', 'paper', 'scissors'];
+const itemSTRING = ITEMS.join(' ').toLocaleLowerCase();
 
-//Computer player chooses game piece
 let computerPlay = ITEMS[Math.floor(Math.random()*ITEMS.length)];
+console.log(computerPlay);
 
-
-//Play a round of the game
-/*
-let playerSelection = 'rock';
-*/
-
-//playerSelection input function
-let playerSelection = prompt('Please choose an item: Rock, Paper or Scissors');
-
-
-//switch function to announce playerSelection
-switch(playerSelection) {
-    case 'rock':
-        console.log("Player chooses Rock!");
-        break;
-    case 'paper':
-        console.log("Player chooses Paper");
-        break;
-    case 'scissors':
-        console.log("Player chooses Scissors!");
-        break;
-}
-const computerSelection = computerPlay;
-switch(computerSelection) {
-    case 'rock':
-        console.log("Computer chooses Rock!");
-        break;
-    case 'paper':
-        console.log("Computer chooses Paper");
-        break;
-    case 'scissors':
-        console.log("Computer chooses Scissors!");
-        break;
-}
-
-function playRound(playerSelection, computerSelection) {
-
-    //if both players choose same item return 'try again'
-    if (playerSelection == computerSelection) {
-        return('Try again!') && prompt('Choose again!');
+function playerSelection(ITEMS) {
+    let input = prompt(`Please choose an item ${ITEMS}.`);
+    let choice = input.toLocaleLowerCase();
+    //make case in sensitive
+    if (choice) {
+        choice === itemSTRING;
+        choice.localeCompare(itemSTRING, undefined, {sensitivity: 'base' });
         
-    } //if player choose 'rock' and computer choose 'paper' return 'game over'
-    else if (computerSelection == 'paper') {
-        return('Game over');
-    } //if player chooses 'rock' and comptuer choose 'scissors' return 'You win!'
-    else if (computerSelection == 'scissors') {
-        return('You win!');
+    }
+      
+    console.log(itemSTRING)
+    //compare player choice to ITEMS
+    if (itemSTRING.includes(choice)) {
+        console.log(`You chose ${choice}`);
+    } else {  
+        console.log(`${choice} is not an item, please try again`);
+        playerSelection(ITEMS);
+    } 
     }
 
-}
-console.log(playRound(playerSelection, computerSelection));
+playerSelection(ITEMS);
