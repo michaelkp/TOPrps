7e055fb513326951b35094c9fb9e6a2e4b5b2145
Michael <michaelk99@gmail.com>
2022-06-10 16:56:48 -0700

    Add computerSelection

--- scripts.js
+++ scripts.js
@@ -1,27 +1,39 @@
 const ITEMS = ['rock', 'paper', 'scissors'];
 const itemSTRING = ITEMS.join(' ').toLocaleLowerCase();
-
-let computerPlay = ITEMS[Math.floor(Math.random()*ITEMS.length)];
+/*
+const computerPlay = ITEMS[Math.floor(Math.random()*ITEMS.length)];
 console.log(computerPlay);
+*/
+//playerChoice function case insensitivity and make first letter uppercase
 
 function playerSelection(ITEMS) {
     let input = prompt(`Please choose an item ${ITEMS}.`);
     let choice = input.toLocaleLowerCase();
+    
     //make case in sensitive
     if (choice) {
         choice === itemSTRING;
-        choice.localeCompare(itemSTRING, undefined, {sensitivity: 'base' });
-        
-    }
-      
-    console.log(itemSTRING)
-    //compare player choice to ITEMS
-    if (itemSTRING.includes(choice)) {
-        console.log(`You chose ${choice}`);
-    } else {  
-        console.log(`${choice} is not an item, please try again`);
-        playerSelection(ITEMS);
-    } 
+        choice.localeCompare(itemSTRING, undefined, {sensitivity: 'base' });   
     }
 
+    //make first letter of choice uppercase
+    const playerChoice = choice[0].toUpperCase() + choice.substring(1).toLowerCase();
+
+    //compare player choice to ITEMS
+    if (itemSTRING.includes(choice)) {
+        console.log(`You chose ${playerChoice}!`);
+    } else {  
+        console.log(`${playerChoice} is not an item, please try again.`);
+        playerSelection(ITEMS);
+    } 
+}
 playerSelection(ITEMS);
+
+//computer chooses item and make it upperCase
+function computerSelection(ITEMS) {
+    const computerPlay = ITEMS[Math.floor(Math.random()*ITEMS.length)];
+    const computerChoice = computerPlay[0].toUpperCase() + computerPlay.substring(1).toLowerCase();
+    console.log(`The computer chose ${computerChoice}!`);
+    
+}
+computerSelection(ITEMS);
